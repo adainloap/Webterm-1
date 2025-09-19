ff9ce441243af7d1b347372684113891f565c5b6
nekoCd <Adamkinville@protonmail.com>
2025-09-19 15:32:37 +0000

    Recaptch ful

---
+++ app/src/main/java/com/onrender/webterm_6e21/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package com.onrender.webterm_6e21.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
